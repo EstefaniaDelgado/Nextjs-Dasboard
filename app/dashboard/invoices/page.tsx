385c1e2f2149f224aa77e1db6cf797b1f15a5780
EstefaniaDelgado <teff_64@hotmail.com>
2024-11-15 08:46:47 -0500

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function InvoicesPage(){
+ return <p>Esta es la pagina que muestra el contenido de Invoices</p>
+}
---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { monserrat } from './ui/fonts';
+import './ui/global.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${monserrat.className} antialiased`}>
+      <header className='text-center bg-blue-300 w-full'>
+        <h2>Esto es el header 😄</h2>
+        </header>
+        
+        {children}
+        <footer className='py-5 flex justify-center bg-blue-300'>Hecho con ♥ por la gente de Vercel</footer>
+        
+        </body>
+
+       
+    </html>
+  );
+}
